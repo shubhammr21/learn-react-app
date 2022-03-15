35ff51b42fe2292c5df9a491b55230c1dbe13b26
Shubham Maurya <shubhammr21@gmail.com>
2022-03-15 08:16:28 +0530

    Added input handlers

--- frontend/src/components/NewExpense/ExpenseForm.js
+++ frontend/src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,41 @@ import React from "react"
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
+  const titleChangeHandler = event => {
+    console.log(event.target.value)
+  }
+  const amountChangeHandler = event => {
+    console.log(event.target.value)
+  }
+  const dateChangeHandler = event => {
+    console.log(event.target.value)
+  }
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" />
+          <input type="text" id="title" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" id="amount" min="0.01" step="0.01" />
+          <input
+            type="number"
+            id="amount"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" min="2021-01-01" max="2022-12-01" />
+          <input
+            type="date"
+            id="date"
+            min="2021-01-01"
+            max="2022-12-01"
+            onChange={dateChangeHandler}
+          />
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
